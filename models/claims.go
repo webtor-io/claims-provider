4e95715492b317f9c73128a5799ca0953255ec41
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2025-12-11 22:27:47 +0300

    refactor: add tableName struct tag to Claims for discarding unknown columns

--- models/claims.go
+++ models/claims.go
@@ -1,11 +1,12 @@
 package models
 
 type Claims struct {
-	Email         string `pg:"email"`
-	PatreonUserID string `pg:"patreon_user_id"`
-	TierID        uint32 `pg:"tier_id"`
-	TierName      string `pg:"tier_name"`
-	DownloadRate  uint64 `pg:"download_rate"`
-	EmbedNoAds    bool   `pg:"embed_noads"`
-	SiteNoAds     bool   `pg:"site_noads"`
+	tableName     struct{} `pg:",discard_unknown_columns"`
+	Email         string   `pg:"email"`
+	PatreonUserID string   `pg:"patreon_user_id"`
+	TierID        uint32   `pg:"tier_id"`
+	TierName      string   `pg:"tier_name"`
+	DownloadRate  uint64   `pg:"download_rate"`
+	EmbedNoAds    bool     `pg:"embed_noads"`
+	SiteNoAds     bool     `pg:"site_noads"`
 }
